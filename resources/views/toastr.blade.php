06cc92df8addf2824e5e6c2629d9e415ac659bfd
A Awal Hadi <awalhadi5@gmail.com>
2024-10-03 22:17:09 +0600

    updatepackahe blade

--- resources/views/toastr.blade.php
+++ resources/views/toastr.blade.php
@@ -1,14 +1,18 @@
 @if(session()->has('toastr'))
-    @if(!Session::has('toastr_loaded'))
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-        @php(Session::put('toastr_loaded', true))
-    @endif
+<!-- @if(!Session::has('toastr_loaded')) -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+@php(Session::put('toastr_loaded', true))
+<!-- @endif -->
 
-    <script>
-        toastr.options = {!! json_encode(config('toastr.options')) !!};
-        @foreach(session('toastr') as $notification)
-            toastr["{{ $notification['type'] }}"]("{{ $notification['message'] }}", "{{ $notification['title'] }}", {!! json_encode($notification['options']) !!});
-        @endforeach
-    </script>
+<script>
+    toastr.options = {
+        !!json_encode(config('toastr.options')) !!
+    };
+    @foreach(session('toastr') as $notification)
+    toastr["{{ $notification['type'] }}"]("{{ $notification['message'] }}", "{{ $notification['title'] }}", {
+        !!json_encode($notification['options']) !!
+    });
+    @endforeach
+</script>
 @endif
