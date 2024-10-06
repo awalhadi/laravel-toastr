30d695d56aac6ce7e4b65aa6d1a62c2746ee5757
A Awal Hadi <awalhadi5@gmail.com>
2024-10-06 06:58:39 +0600

    update blade data

--- resources/views/toastr.blade.php
+++ resources/views/toastr.blade.php
@@ -1,8 +1,5 @@
 @if(session()->has('toastr'))
-<!-- @if(!Session::has('toastr_loaded')) -->
 <script src="https://raw.githubusercontent.com/awalhadi/easy-toast-js/refs/heads/main/dist/easyToast.min.js"></script>
-<!-- @php(Session::put('toastr_loaded', true)) -->
-<!-- @endif -->
 
 <script>
     const toast = new ToastManager({
